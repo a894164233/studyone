d43d989ec50073c04b72eb9e1d66077d96e5b9cd
baitao <baitao123>
2018-06-29 18:21:47 +0800

    first

---
+++ src/main/java/com/example/studyone/StudyoneApplication.java
@@ -0,0 +1,12 @@
+package com.example.studyone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyoneApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/studyone/StudyoneApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.studyone;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudyoneApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
